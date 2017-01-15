45af72de30a2fffa84e68a63efd3b024c786d9b1
risnawan <risnawanbudianto@gmail.com>
2017-01-15 12:33:21 +0700

    tim
    masih belajar

--- src/projectkursuslepkom/ProjectKursusLePKom.java
+++ src/projectkursuslepkom/ProjectKursusLePKom.java
@@ -16,6 +16,7 @@ public class ProjectKursusLePKom {
      */
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hello world");
     }
     
 }

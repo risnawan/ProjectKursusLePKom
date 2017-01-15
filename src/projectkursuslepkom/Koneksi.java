ba1f55f8c2b9a41692e53a91b1ffeb9166de055c
risnawan <risnawanbudianto@gmail.com>
2017-01-15 19:51:45 +0700

    tim

---
+++ src/projectkursuslepkom/Koneksi.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectkursuslepkom;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Risnawan
+ */
+public class Koneksi {
+    private Statement data = null;
+    private Connection koneksi = null;
+    
+    private String Url = "jdbs:mysql://localhost:3360/ragunan";
+    private String DBUser = "root";
+    private String DBPass = "";
+
+    public Koneksi(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            koneksi = DriverManager.getConnection(Url, DBUser, DBPass);
+            data = this.koneksi.createStatement();
+        }
+        catch(Throwable ex){
+            System.out.println("Error : "+ex);
+            System.exit(1);
+        } 
+    }
+    
+    public Connection getConnection(){
+        return koneksi;
+    }
+    
+    public Statement getStatement(){
+        return data;
+    }
+}
